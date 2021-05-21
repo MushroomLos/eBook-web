aad7071fdf48e2b8ad139d294c19845d7b50ec0a
MushroomLos <mushroomlos@outlook.com>
2021-05-21 20:14:34 +0800

    first test

---
+++ src/pages/About/About.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <h1>关于</h1>
+        <hr>
+        <div>
+            <div>
+                <span>
+                    Some advice? Welcome to issue.
+                    <i class="iconfont icon-github"></i>
+                    <a href="https://www.github.com/liuzhengwei127/EBook">五柳寄书的EBook</a>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "About"
+    }
+</script>
+
+<style scoped>
+
+</style>
